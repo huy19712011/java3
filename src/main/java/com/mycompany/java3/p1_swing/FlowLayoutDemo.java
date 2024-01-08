01d56e36091fe73c1a752eedd5c3af536bdf1aeb
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2024-01-08 14:39:28 +0700

    FlowLayout demo

---
+++ src/main/java/com/mycompany/java3/p1_swing/FlowLayoutDemo.java
@@ -0,0 +1,52 @@
+package com.mycompany.java3.p1_swing;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class FlowLayoutDemo {
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        // default = CENTER; hor, ver spacing = 10, 10
+        frame.setLayout(new FlowLayout(FlowLayout.LEADING, 10, 10));
+
+        // step 2
+        JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(250, 250));
+        panel.setBackground(Color.lightGray);
+        panel.setLayout(new FlowLayout());
+
+// step 1
+//        frame.add(new JButton("1"));
+//        frame.add(new JButton("2"));
+//        frame.add(new JButton("3"));
+//        frame.add(new JButton("4"));
+//        frame.add(new JButton("5"));
+//        frame.add(new JButton("6"));
+//        frame.add(new JButton("7"));
+//        frame.add(new JButton("8"));
+//        frame.add(new JButton("9"));
+
+        // step 2
+        panel.add(new JButton("1"));
+        panel.add(new JButton("2"));
+        panel.add(new JButton("3"));
+        panel.add(new JButton("4"));
+        panel.add(new JButton("5"));
+        panel.add(new JButton("6"));
+        panel.add(new JButton("7"));
+        panel.add(new JButton("8"));
+        panel.add(new JButton("9"));
+
+
+        frame.add(panel);
+        frame.setVisible(true);
+    }
+}
