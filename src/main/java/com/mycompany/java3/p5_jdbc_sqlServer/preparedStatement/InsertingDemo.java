4aa25dc738526f68709ee92cd26f41776a0d45c6
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2024-01-19 10:08:29 +0700

    Prepared Statement: Inserting rows

--- src/main/java/com/mycompany/java3/p5_jdbc_sqlServer/preparedStatement/InsertingDemo.java
+++ src/main/java/com/mycompany/java3/p5_jdbc_sqlServer/preparedStatement/InsertingDemo.java
@@ -1,13 +1,13 @@
 package com.mycompany.java3.p5_jdbc_sqlServer.preparedStatement;
 
 import com.mycompany.java3.p5_jdbc_sqlServer.DatabaseConnectionManager_SQLServer;
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
-public class CreateTableDemo {
+public class InsertingDemo {
 
     public static void main(String[] args) {
 
@@ -23,17 +23,18 @@ public class CreateTableDemo {
             connection = dcm.getConnection();
 
             String sql = """
-                            CREATE TABLE employee2(
-                               Id INT NOT NULL,
-                               Name NVARCHAR(30) NOT NULL,
-                               Salary DECIMAL NOT NULL,
-                               PRIMARY KEY (ID)
-                            );
+                            INSERT INTO employee2(Id, Name, Salary)
+                            VALUES(?, ?, ?);
                          """;
 
             preparedStatement = connection.prepareStatement(sql);
 
-            preparedStatement.execute();
+            // set values
+            preparedStatement.setInt(1, 1000);
+            preparedStatement.setString(2, "John");
+            preparedStatement.setDouble(3, 50000.5);
+
+            preparedStatement.executeUpdate();
 
             System.out.println("done...");
 
